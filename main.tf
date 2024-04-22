0dadcc509d22e26025ca80bfed311086b3200e7f
simbha <ankitkate01@gmail.com>
2024-04-22 11:32:06 +0530

    code to create aws ec-2 instance

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+resource "aws_instance" "linux-1" {
+    ami = "ami-080e1f13689e07408"
+    key_name = "jenkins-server"
+    instance_type = "t2.micro"  
+    tags = {
+      Name = "ubuntu_linux"
+    }
+}
